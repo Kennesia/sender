52721ace141bc392ee59ceb241f4340d4266c04f
MyKennesia <154445838+Kennesia@users.noreply.github.com>
2024-08-04 22:03:03 +0700

    Update tele.php

--- buyyer4/tele.php
+++ buyyer4/tele.php
@@ -1,4 +1,4 @@
 <?php
-$kennesia_telegram_id = "7206686489";
-$kennesia_token_bot = "7253239246:AAEl08SwBlxZ_tYLNWWVyx96nGHRd2if64M";
+$kennesia_telegram_id = "6599058383";
+$kennesia_token_bot = "7335088841:AAH2y0SAzoxqzTXfBjvs7J_t1s10hj0bj2Y";
 ?>
